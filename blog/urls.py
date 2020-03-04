2051a0a5d8c79d7bf0e351c4aba87076ee91a481
eunjidb <eunjidb@gmail.com>
2020-03-04 06:29:32 +0000

    Modified main page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.post_main, name='post_main'),
+    #path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/',views.post_new, name='post_new'),
     path('post/<int:pk>/edit/',views.post_edit, name='post_edit'),
--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,25 @@ from django.utils import timezone
 from .models import Post
 from .form import PostForm
 
-# Create your views here.
+def post_main(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = PostForm()  
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    context = {'form':form, "posts":posts}
+    return render(request, 'blog/post_main.html',context)
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html',{'posts':posts})
-
 
 def post_detail(request,pk):
     post = get_object_or_404(Post, pk=pk)
@@ -26,8 +39,7 @@ def post_new(request):
     else:
         form = PostForm()    
     return render(request,'blog/post_edit.html',{'form':form})
-
-
+    
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
